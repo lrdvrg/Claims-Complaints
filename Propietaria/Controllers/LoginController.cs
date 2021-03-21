93893c6877e513693f230145da0826a283466e25
Isai Vargas <isaidvargas@hotmail.com>
2021-03-20 23:38:12 -0400

    feature: usuarios y roles

---
+++ Propietaria/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Propietaria.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            try
+            {
+                using (Models.ReclaimsAndComplaints2Entities db = new Models.ReclaimsAndComplaints2Entities())
+                {
+                    var oUser = (from d in db.Users
+                                 where d.Email == email && d.Password == password
+                                 select d).FirstOrDefault();
+
+                    if (oUser == null)
+                    {
+                        ViewBag.Error = "Datos incorrectos o usuario no existe.";
+                        return View();
+                    }
+
+                    Session["User"] = oUser;
+                }
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ Propietaria/Filters/CheckSession.cs
@@ -0,0 +1,19 @@
+﻿using Propietaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Propietaria.Filters
+{
+    public class CheckSession : ActionFilterAttribute
+    {
+        private Users oUser;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
